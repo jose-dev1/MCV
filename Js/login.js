f97c56fa80981554c5bca8891bfc73f71ed85f07
jose-dev1 <jose.cm25@hotmail.com>
2023-05-30 18:43:09 -0500

    cambios en la version de sweetAlert y modificacion de archivos

--- Js/login.js
+++ Js/login.js
@@ -1,20 +1,56 @@
-const inputs = document.querySelectorAll(".input");
-
-
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
+function loginsucc(){
+	Swal.fire({
+		title: 'Logeando!',
+		html: 'Validando informacion en 2 segundo',
+		timer: 2000,
+		timerProgressBar: true,
+		didOpen: () => {
+		  Swal.showLoading()
+		  const b = Swal.getHtmlContainer().querySelector('b')
+		  timerInterval = setInterval(() => {
+			b.textContent = Swal.getTimerLeft()
+		  }, 100)
+		},
+		willClose: () => {
+		  clearInterval(timerInterval)
+		}
+	  }).then((result) => {
+		if (result.dismiss === Swal.DismissReason.timer) {
+		  console.log('I was closed by the timer')
+		}
+	  })
 }
-
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
+function rpass(){
+	Swal.fire({
+		title: 'Digite su correo electronico',
+		input: 'text',
+		inputAttributes: {
+		  autocapitalize: 'off'
+		},
+		showCancelButton: true,
+		confirmButtonText: 'Enviar',
+		showLoaderOnConfirm: true,
+		preConfirm: (login) => {
+		  return fetch(`//api.github.com/users/${login}`)
+			.then(response => {
+			  if (!response.ok) {
+				throw new Error(response.statusText)
+			  }
+			  return response.json()
+			})
+			.catch(error => {
+			  Swal.showValidationMessage(
+				`Request failed: ${error}`
+			  )
+			})
+		},
+		allowOutsideClick: () => !Swal.isLoading()
+	  }).then((result) => {
+		if (result.isConfirmed) {
+		  Swal.fire({
+			title: `${result.value.login}'s avatar`,
+			imageUrl: result.value.avatar_url
+		  })
+		}
+	  })
 }
-
-
-inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
-});

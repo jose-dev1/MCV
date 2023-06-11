a74492974c4071256fd85b595a0799de9745f9e6
jose-dev1 <jose.cm25@hotmail.com>
2023-06-10 21:59:48 -0500

    se creo la historia clinica con sus animaciones

---
+++ Js/alerta_historiac.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    $('#tablaHistoria').DataTable({ 
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json',
+        },
+    });
+});
+
+
+function actualizar (){
+    Swal.fire({
+        title: 'Desea guardar los cambios?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Guardar',
+        denyButtonText: `No guardar`,
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire('Guardado!', '', 'success')
+        } else if (result.isDenied) {
+          Swal.fire('Los cambios no se guardaron', '', 'info')
+        }
+      })
+}
+
+function alerta () {
+    Swal.fire(
+        'Felicidades!',
+        'se ha guardado la historia clinica!',
+        'success'
+    );
+  }
+  
+function progres() {
+ let timerInterval
+  Swal.fire({
+  title: 'Este archivo se descargara en pdf',
+  html: 'Descargando en 2 segundos',
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
+}
+

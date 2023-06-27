ceb3334c30e34a267fe20f3bcfeb39428d2a85e4
samuel <samivazqueles@gmail.com>
2023-06-27 09:19:53 -0500

    correciones de vacunas , servicios groomer

--- Js/DashBoard/Groomer/Servicios.js
+++ Js/DashBoard/Groomer/Servicios.js
@@ -1,28 +1,29 @@
-const btnsdeClinar = document.querySelectorAll("#sweet-btn")
+const eliminarDatos = document.querySelectorAll("#borrarDatos");
 
-btnsdeClinar.forEach(button =>{
-
-    button.addEventListener("click",()=> {
-        Swal.fire({
-            title: '¿Quiere Terminar El Servicio?',
-            showDenyButton: true,
-            confirmButtonText: 'Terminar',
-            denyButtonText: `Cancelar`,
-          }).then((result) => {
-            /* Read more about isConfirmed, isDenied below */
-            if (result.isConfirmed) {
-              Swal.fire('Se Notifico al Cliente', '', 'success')
-            } else if (result.isDenied) {
-              Swal.fire('Cancelado', '', 'info')
-            }
-          })
-    });
-})
 
 $(document).ready(function () {
-  $('#tableServiciosA').DataTable({ 
-      language: {
-          url: 'https://cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json',
-      },
+  $('#tableServiciosA').DataTable({
+    language: {
+      url: 'https://cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json',
+    },
+  });
+});
+
+
+
+eliminarDatos.forEach(botones => {
+  botones.addEventListener("click",()=>{
+      Swal.fire({
+          title: 'Quiere eliminar el examen de esta mascota?',
+          showDenyButton: true,
+          confirmButtonText: 'Confirmar',
+          denyButtonText: `Canselar`,
+        }).then((result) => {
+          if (result.isConfirmed) {
+            Swal.fire('Eliminado', '', 'success')
+          } else if (result.isDenied) {
+            Swal.fire('Cancelado ', '', 'info')
+          }
+        });
   });
 });
--- Js/DashBoard/Veterinario/Vacunacion.js
+++ Js/DashBoard/Veterinario/Vacunacion.js
@@ -1,3 +1,6 @@
+const eliminarDatos = document.querySelectorAll("#borrarDatos");
+
+
 $(document).ready(function () {
     $('#TablaVacunacion').DataTable({ 
         language: {
@@ -8,5 +11,22 @@ $(document).ready(function () {
         language: {
             url: 'https://cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json',
         },
+    });
+});
+
+eliminarDatos.forEach(botones => {
+    botones.addEventListener("click",()=>{
+        Swal.fire({
+            title: 'Quiere eliminar el examen de esta mascota?',
+            showDenyButton: true,
+            confirmButtonText: 'Confirmar',
+            denyButtonText: `Cancelar`,
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire('Eliminado', '', 'success')
+            } else if (result.isDenied) {
+              Swal.fire('Cancelado ', '', 'info')
+            }
+          });
     });
 });

040a76a9e82e06fb26652480506a2445443fadad
jose-dev1 <jose.cm25@hotmail.com>
2023-12-13 13:36:42 -0500

    se resolvio el error de examenes

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import Nuevapw from "./page/validarContraseña";
 import Perfil from "./page/cliente/perfil";
 import AddEmploye from "./page/admin/homeAdmin";
 import Inicio from "./page/veterinario/home.jsx";
-import Hospitalizaciones from './page/veterinario/hospitalizaciones.jsx'
+import Hospitalizaciones from "./page/veterinario/hospitalizaciones.jsx";
 import HomeAuxiliar from "./page/auxiliar/home.jsx";
 import HomeGroomer from "./page/groomer/home.jsx";
 import Mascota from "./page/cliente/mascota.jsx";
-import DescargarCerficado from "./page/cliente/descargarCertificado"
-import DescargarExamen from "./page/cliente/descargarExamen"
+import DescargarCerficado from "./page/cliente/descargarCertificado";
+import DescargarExamen from "./page/cliente/descargarExamen";
 import { ProtectedRoute } from "./routes/routes.js";
 import Examenes from "./page/veterinario/examenes.jsx";
 

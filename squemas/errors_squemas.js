cef4c53dc4473d8c7291f5eb3d96a751949d0bac
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-02-21 14:28:00 -0500

    Nuevo tipo  de error creado

--- squemas/errors_squemas.js
+++ squemas/errors_squemas.js
@@ -1,12 +1,13 @@
 const createErrorFactory = function (name) {
   return class BusinessError extends Error {
-    constructor(message) {
-      super(message);
-      this.name = name;
+    constructor (message) {
+      super(message)
+      this.name = name
     }
-  };
-};
+  }
+}
 
-export const ConnectionError = createErrorFactory("ConnectionError");
-export const NotFoundUser = createErrorFactory("User not found");
-export const InvalidCredential = createErrorFactory("invalid Credential");
+export const ConnectionError = createErrorFactory('ConnectionError')
+export const NotFoundUser = createErrorFactory('User not found')
+export const InvalidCredential = createErrorFactory('invalid Credential')
+export const NoDataFound = createErrorFactory('Data not found')

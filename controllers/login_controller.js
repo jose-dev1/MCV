baa9c101f5834d6944b06964b46d4a0a67a40786
jose-dev1 <jose.cm25@hotmail.com>
2024-02-21 12:56:22 -0500

    modificacion de variables de login,login funcionando

--- controllers/login_controller.js
+++ controllers/login_controller.js
@@ -1,23 +1,24 @@
-import { LoginModel } from '../models/login_model.js'
-import { NotFoundUser, InvalidCredential } from '../squemas/errors_squemas.js'
+import { LoginModel } from "../models/login_model.js";
+import { NotFoundUser, InvalidCredential } from "../squemas/errors_squemas.js";
 
 export class LoginController {
-  static async login (req, res) {
-    const { userCorreo, userPassword } = req.body
-    const response = await LoginModel.login({ userCorreo, userPassword })
+  static async login(req, res) {
+    const { userCorreo, userPassword } = req.body;
+
+    const response = await LoginModel.login({ userCorreo, userPassword });
     if (response instanceof NotFoundUser) {
-      res.status(404).json({ menssage: 'Usuario no registrado' })
+      res.status(404).json({ menssage: "Usuario no registrado" });
     } else if (response instanceof InvalidCredential) {
-      res.status(400).json({ menssage: 'Credenciales invalidas' })
+      res.status(400).json({ menssage: "Credenciales invalidas" });
     } else if (response instanceof Error) {
-      res.status(500).json({ message: 'Error interno del servidor ' })
+      res.status(500).json({ message: "Error interno del servidor " });
     } else {
       res.json({
         success: true,
-        role: response.fk_tipo_usuario,
+        role: response.id_tipo_usuario,
         user: response,
-        message: 'Inicio de sesion exitoso'
-      })
+        message: "Inicio de sesion exitoso",
+      });
     }
   }
 }
--- squemas/errors_squemas.js
+++ squemas/errors_squemas.js
@@ -1,12 +1,12 @@
 const createErrorFactory = function (name) {
   return class BusinessError extends Error {
-    constructor (message) {
-      super(message)
-      this.name = name
+    constructor(message) {
+      super(message);
+      this.name = name;
     }
-  }
-}
+  };
+};
 
-export const ConnectionError = createErrorFactory('ConnectionError')
-export const NotFoundUser = createErrorFactory('User not found')
-export const InvalidCredential = createErrorFactory('invalid Credential')
+export const ConnectionError = createErrorFactory("ConnectionError");
+export const NotFoundUser = createErrorFactory("User not found");
+export const InvalidCredential = createErrorFactory("invalid Credential");

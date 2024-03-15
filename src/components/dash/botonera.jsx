cb9a27d40b8b89a70d8707a0bb25e30830e8ed5f
Kilerwin <miguel642013@gmail.com>
2024-03-15 12:46:24 -0500

    Se cambia estilos del titulo botonera al formato establecido

--- src/components/dash/botonera.jsx
+++ src/components/dash/botonera.jsx
@@ -13,7 +13,7 @@ export default function Botonera (props) {
           }
         }}
       >
-        <h1 className='text-center content-center text-5xl'>{title}</h1>
+         <h2 className="mb-1 mt-1 text-4xl font-bold bg-gradient-to-r from-purple-300 to-purple-700 text-white p-2 rounded-lg">{title}</h2>
         <ButtonGroup className='justify-end' variant='outlined' aria-label='outlined button group'>
           {agregar}
           {editar}

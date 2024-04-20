e70ba05d8cefa665e5c4bce4904b5c39d8228d66
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-04-20 18:09:16 -0500

    🎨 Se anaden validaciones
    En los esquemas del empleado se anaden validaciones para evitar caracteres especiales a la hora de registrar o editar a un  empleado

--- squemas/admin.js
+++ squemas/admin.js
@@ -2,13 +2,21 @@ import { z } from 'zod'
 
 const adminCreateSchema = z.object({
 
-  primerNombreEmpleado: z.string().min(1, { message: 'El primer nombre del empleado no puede estar vacío.' }),
-  segundoNombreEmpleado: z.string({ message: 'El segundo nombre del empleado tiene que ser texto.' }),
-  primerApellidoEmpleado: z.string().min(1, { message: 'El primer apellido del empleado no puede estar vacío.' }),
-  segundoApellidoEmpleado: z.string().min(1, { message: 'El segundo apellido del empleado no puede estar vacío.' }),
-  numeroDocumentoEmpleado: z.string().min(7, { message: 'El número de documento del empleado debe tener entre 7 y 10 caracteres.' }).max(10, { message: 'El número de documento del empleado debe tener entre 7 y 10 caracteres.' }).refine(value => /^[a-zA-Z0-9]+$/.test(value), {
-    message: 'El número de documento del empleado no puede contener caracteres especiales.'
-  }),
+  primerNombreEmpleado: z.string()
+    .min(1, { message: 'El primer nombre del empleado no puede estar vacío.' })
+    .refine(value => /^[a-zA-Z]+$/.test(value), { message: 'El primer nombre del empleado no puede contener caracteres especiales.' }),
+  segundoNombreEmpleado: z.string({ message: 'El segundo nombre del empleado tiene que ser texto.' })
+    .refine(value => /^[a-zA-Z]+$/.test(value), { message: 'El segundo nombre del empleado no puede contener caracteres especiales.' }),
+  primerApellidoEmpleado: z.string()
+    .min(1, { message: 'El primer apellido del empleado no puede estar vacío.' })
+    .refine(value => /^[a-zA-Z]+$/.test(value), { message: 'El primer apellido del empleado no puede contener caracteres especiales.' }),
+  segundoApellidoEmpleado: z.string()
+    .min(1, { message: 'El segundo apellido del empleado no puede estar vacío.' })
+    .refine(value => /^[a-zA-Z]+$/.test(value), { message: 'El segundo apellido del empleado no puede contener caracteres especiales.' }),
+  numeroDocumentoEmpleado: z.string()
+    .min(7, { message: 'El número de documento del empleado debe tener entre 7 y 10 caracteres.' })
+    .max(10, { message: 'El número de documento del empleado debe tener entre 7 y 10 caracteres.' })
+    .refine(value => /^[a-zA-Z0-9]+$/.test(value), { message: 'El número de documento del empleado no puede contener caracteres especiales.' }),
   idTipoDocumento: z.string().min(1, { message: 'Tiene que escoger el tipo de documento para el empleado' }),
   passwordUsuario: z.string().min(1, { message: 'La contraseña del usuario no puede estar vacía.' }),
   correoUsuario: z.string().email({ message: 'El correo no esta correctamente escrito' }).min(1, { message: 'El correo del usuario no puede estar vacío.' }),
@@ -17,10 +25,17 @@ const adminCreateSchema = z.object({
 })
 
 const adminUpdateSchema = z.object({
-  primerNombreEmpleado: z.string().min(1, { message: 'El primer nombre del empleado no puede estar vacío.' }),
-  segundoNombreEmpleado: z.string({ message: 'El segundo nombre del empleado tiene que ser texto.' }),
-  primerApellidoEmpleado: z.string().min(1, { message: 'El primer apellido del empleado no puede estar vacío.' }),
-  segundoApellidoEmpleado: z.string().min(1, { message: 'El segundo apellido del empleado no puede estar vacío.' }),
+  primerNombreEmpleado: z.string()
+    .min(1, { message: 'El primer nombre del empleado no puede estar vacío.' })
+    .refine(value => /^[a-zA-Z]+$/.test(value), { message: 'El primer nombre del empleado no puede contener caracteres especiales.' }),
+  segundoNombreEmpleado: z.string({ message: 'El segundo nombre del empleado tiene que ser texto.' })
+    .refine(value => /^[a-zA-Z]+$/.test(value), { message: 'El segundo nombre del empleado no puede contener caracteres especiales.' }),
+  primerApellidoEmpleado: z.string()
+    .min(1, { message: 'El primer apellido del empleado no puede estar vacío.' })
+    .refine(value => /^[a-zA-Z]+$/.test(value), { message: 'El primer apellido del empleado no puede contener caracteres especiales.' }),
+  segundoApellidoEmpleado: z.string()
+    .min(1, { message: 'El segundo apellido del empleado no puede estar vacío.' })
+    .refine(value => /^[a-zA-Z]+$/.test(value), { message: 'El nombre del empleado no puede contener caracteres especiales.' }),
   idGenero: z.string().min(1, { message: 'Tiene que escoger un genero para el empleado' }),
   correoUsuario: z.string().email({ message: 'El correo no esta correctamente escrito' }).min(1, { message: 'El correo del usuario no puede estar vacío.' }),
   numeroDocumentoEmpleado: z.string().min(7).max(10, { message: 'El número de documento del empleado debe tener entre 7 y 10 caracteres.' }).refine(value => /^[a-zA-Z0-9]+$/.test(value), {

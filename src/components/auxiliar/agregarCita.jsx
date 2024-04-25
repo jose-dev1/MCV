f35f03b5794c9235cf5f6dc97156430d59156a3c
Kilerwin <miguel642013@gmail.com>
2024-04-25 08:33:19 -0500

    Cambio estilo boton registrar en agendar cita Se crea prototipo de estilos para el boton de registrar con el fin de identificar visualmenter cuando esta bloqueado

--- src/components/auxiliar/agregarCita.jsx
+++ src/components/auxiliar/agregarCita.jsx
@@ -405,7 +405,9 @@ export const Maurisio = (props) => {
                         <Grid item xs={12}>
                             <button
                                 type='submit'
-                                className='w-full inline-block px-6 py-3 font-bold text-center text-white uppercase align-middle transition-all rounded-lg cursor-pointer bg-gradient-to-tl from-blue-500 to-violet-500 leading-normal text-xs ease-in tracking-tight-rem shadow-xs bg-150 bg-x-25 hover:-translate-y-px active:opacity-85 hover:shadow-md'
+                                className={`w-full inline-block px-6 py-3 font-bold text-center text-white uppercase align-middle transition-all rounded-lg 
+                                cursor-pointer bg-gradient-to-tl from-blue-500 to-violet-500 leading-normal text-xs ease-in tracking-tight-rem 
+                                shadow-xs bg-150 bg-x-25 hover:-translate-y-px active:opacity-85 hover:shadow-md ${disableBoton ? 'opacity-50': ''}`}
                                 disabled={disableBoton}
                             >
                                 Registrar

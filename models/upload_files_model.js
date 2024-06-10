f8c4b782f72a4db09f0a6dc429a28fc27fbf1da6
Kilerwin <miguel642013@gmail.com>
2024-06-10 17:39:43 -0500

    Refactor upload files model Se refactoriza el modelo de carga de archivos para hacerlo mas amigable con los recursos gastados y evitar problemas en algunos servidores

--- models/upload_files_model.js
+++ models/upload_files_model.js
@@ -1,22 +1,13 @@
 import { storage } from './connection_firebase.js'
-import fs from 'fs'
-import { fileURLToPath } from 'url'
-import path from 'path'
 import { getDownloadURL, uploadBytes, ref } from 'firebase/storage' // Importar las funciones necesarias de Firebase Storage
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export class UploadFilesModel {
   static async uploadExams (files, dir) {
     try {
       const { archivo } = files
-      const uploadPath = path.join(__dirname, '../temp', archivo.name)
-      await archivo.mv(uploadPath)
       const storageRef = ref(storage, `${dir}/${Date.now()}${archivo.name}`)
-      await uploadBytes(storageRef, fs.readFileSync(uploadPath))
+      await uploadBytes(storageRef, archivo.data)
       const downloadURL = await getDownloadURL(storageRef)
-      fs.unlinkSync(uploadPath)
       return downloadURL
     } catch (error) {
       console.log(error)
